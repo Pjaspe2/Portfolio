f052f37cee72a9fd3d4ccd585b0234de06bf3994
Patrick Jasper <patrickjasper@MacBookPro.lan>
2025-06-10 23:15:14 -0500

    Added gtag for sight traffic analytics

--- app/lib/gtag.ts
+++ app/lib/gtag.ts
@@ -1,11 +1,13 @@
 // Google Analytics measurement ID
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID
+export const GA_MEASUREMENT_ID = 'G-G1G9SEBVGC'
 
 // Log page views
 export const pageview = (url: string) => {
-  window.gtag('config', GA_MEASUREMENT_ID as string, {
-    page_path: url,
-  })
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    })
+  }
 }
 
 // Log specific events
@@ -15,9 +17,11 @@ export const event = ({ action, category, label, value }: {
   label: string
   value?: number
 }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  })
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    })
+  }
 } 
